d21a88ba1fbfa0fa6217cb505994f4d691a30f3d
yangyang.niu <yangyang.niu@leediancn.com>
2016-05-15 14:08:16 +0800

    评论模块

---
+++ Journal_Niu/Classes/Zhitanhui/PinglunViewController.h
@@ -0,0 +1,13 @@
+//
+//  PinglunViewController.h
+//  Journal_Niu
+//
+//  Created by WP on 16/5/15.
+//  Copyright © 2016年 yangyang.niu. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface PinglunViewController : RootViewController
+
+@end
---
+++ Journal_Niu/Classes/Zhitanhui/PinglunViewController.m
@@ -0,0 +1,38 @@
+//
+//  PinglunViewController.m
+//  Journal_Niu
+//
+//  Created by WP on 16/5/15.
+//  Copyright © 2016年 yangyang.niu. All rights reserved.
+//
+
+#import "PinglunViewController.h"
+
+@interface PinglunViewController ()
+
+@end
+
+@implementation PinglunViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSString * str = [@"" stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
